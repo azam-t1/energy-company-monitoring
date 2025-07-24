23c735d1c7e4fcb0a437875a003977ea21da18cb
Azam Turgunboev <azam_turgunboev@epam.com>
2025-07-25 02:06:07 +0500

    remove unnecessary blank line in upload result summary

--- client/src/components/MeterReadingUpload.js
+++ client/src/components/MeterReadingUpload.js
@@ -84,7 +84,7 @@ const MeterReadingUpload = () => {
 
             <p><strong>Successful readings:</strong> {result.successfulReadings}</p>
             <p><strong>Failed readings:</strong> {result.failedReadings}</p>
-            
+
           </div>
           
           {result.errors && result.errors.length > 0 && (
